beae0e12d79be8340fa28c4acb5c9cfd060c7891
Your Name <you@example.com>
2026-08-20 13:40:59 -0300

    Adicao de branch exercicios

--- src/Welcome1.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public class Welcome1 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the mato");
-
-    }
-}
